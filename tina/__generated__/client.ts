a66326a792deaa4be313e63d7a5a0d4086ccfd8a
Chad Moravec <dahc.reverse@gmail.com>
2026-06-27 11:44:16 -0600

    Remove tina/__generated__ from tracking, fix gitignore

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types.js";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '89247a88742beae4e61307f66852731e0b20f02e', queries,  });
-export default client;
-  
